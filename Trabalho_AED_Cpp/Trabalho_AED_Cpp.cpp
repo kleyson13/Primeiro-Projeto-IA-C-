71067930f0f729df8d4d415b1ebeafe045edacfb
Kleyson Mariano <kleysons913@gmail.com>
2021-12-07 16:57:24 -0300

    Criação das principais funções

--- Trabalho_AED_Cpp/Trabalho_AED_Cpp.cpp
+++ Trabalho_AED_Cpp/Trabalho_AED_Cpp.cpp
@@ -1,11 +1,10 @@
-// Trabalho_AED_Cpp.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <speechapi_cxx.h>
 #include <locale.h>
+#include <time.h>
+#include <Windows.h>
 
 using namespace std;
 using namespace Microsoft::CognitiveServices::Speech;
@@ -22,48 +21,120 @@ void texto_em_fala(string Texto)
     requisicao_textofala->SpeakTextAsync(Texto).get();
 }
 
-string fala_em_texto(){
+string fala_em_texto()
+{
     auto resultado = requisicao_falatexto->RecognizeOnceAsync().get();
-    cout << resultado->Text+ "\n";
+    cout << resultado->Text + "\n";
     return resultado->Text;
+}
+
+void repeticao_instrucoes()
+{
+    void instrucoes();
+    void jogo_iniciado();
+
+    system("cls");
+    texto_em_fala("\nPrecisa ouvir as regras mais uma vez?");
+    string opcao = fala_em_texto();
+
+    if (opcao == ("Sim.") || opcao == ("Preciso."))
+    {
+        instrucoes();
+        opcao = "";
+    }
+    else {
+        jogo_iniciado();
+    }
+}
+
+void instrucoes()
+{
+    void repeticao_instrucoes();
+
+    system("cls");
+
+    texto_em_fala("---------------INSTRUÇÕES---------------\n\n");
+    texto_em_fala("- O objetivo do jogo é falar a maior quantidade de palavras que comecem com uma determinada letra, \nsem repetir a mesma palavra.\n");
+    texto_em_fala("- Eu vou escolher uma letra aleatoriamente e todas as palavras deverão iniciar com essa letra.\n");
+    texto_em_fala("- Uma vez eu falo, outra vez você.\n");
+    texto_em_fala("- Cada palavra correta, corresponde a 5 pontos.\n");
+    texto_em_fala("- Se ficar mais de 5 segundos sem falar a palavra, perde o jogo e é exibido a quantidade de pontos e a maior pontuação já alcançada.\n");
+
+    repeticao_instrucoes();
+}
+
+int escolhe_letra()
+{
+    int letra = 0;
+    srand(time(NULL));
+    letra = rand() % 26;
+    return (letra);
+}
+
+void jogo_iniciado()
+{
+    texto_em_fala("Okay, vamos iniciar o jogo!");
+    system("cls");
+}
+
+void inicio()
+{
+    texto_em_fala("-----------JOGO DE PALAVRAS!-----------");
+    texto_em_fala("\nQual é o seu nome?\n");
+    string nome = fala_em_texto();
+    texto_em_fala("\nOlá " + nome + " Vamos jogar um jogo?\n");
+    string opcao = fala_em_texto();
+
+    if (opcao == ("Sim.") || opcao == ("Claro.") || opcao == ("Com certeza."))
+    {
+        texto_em_fala("\nÓtimo, quer que eu te passe as instruções?");
+        opcao = fala_em_texto();
+        if (opcao == ("Sim.") || opcao == ("Claro.") || opcao == ("Com certeza."))
+        {
+            instrucoes();
+        }
+        else
+        {
+            jogo_iniciado();
+        }
+    }
+    else
+    {
+        texto_em_fala("Que pena! Deseja sair?");
+        string reinicio = fala_em_texto();
+        if (reinicio == ("Sim.") || reinicio == ("Claro.") || reinicio == ("Com certeza."))
+        {
+            texto_em_fala("Até logo.");
+            system("cls");
+        }
+    }
 }
 
 int main()
 {
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, ".UTF8");
     autenticacao->SetSpeechRecognitionLanguage("pt-BR");
     autenticacao->SetSpeechSynthesisLanguage("pt-BR");
-    autenticacao->SetSpeechSynthesisVoiceName("pt-BR-Daniel");
+    autenticacao->SetSpeechSynthesisVoiceName("pt-BR-FranciscaNeural");
     requisicao_textofala = SpeechSynthesizer::FromConfig(autenticacao);
     requisicao_falatexto = SpeechRecognizer::FromConfig(autenticacao, audioConfig);
 
     try
     {
-        texto_em_fala("SISTEMA LIGADO");
-        texto_em_fala("Qual é o seu nome?");
-        string nome = fala_em_texto();
-        texto_em_fala("Olá "+nome+"! Informe a sua senha: ");
-        string senha = fala_em_texto();
-        texto_em_fala("Verificando. . .");
+        inicio();
 
-        if (senha == ("123456.")){
-            texto_em_fala("ACESSO CONCEDIDO!");
-        }
-        else{
-            texto_em_fala("ACESSO NEGADO!");
-        }
     }
-    catch(exception e)
+    catch (exception e)
     {
         cout << e.what();
     }
-    return(0);
+    return (0);
 }
 
 // Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
 // Depurar programa: F5 ou menu Depurar > Iniciar Depuração
 
-// Dicas para Começar: 
+// Dicas para Começar:
 //   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
 //   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
 //   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
